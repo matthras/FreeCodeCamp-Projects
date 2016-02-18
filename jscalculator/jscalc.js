529d7b8844416f4f800f13b7c71ed24652834e58
matthras <matthew.y.p.mack@gmail.com>
2016-02-17 18:35:08 -0800

    Started jscalculator

--- jscalculator/jscalc.js
+++ jscalculator/jscalc.js
@@ -1,4 +1,20 @@
 $(document).ready( function() {
+
+	// Display Array
+	var dArray = [];
+
+	// onClick function for each button
+	var dUpdate = function(val) {
+		dArray.push(val);
+	}
+
+	// Evaluate function for equals button
+	var dEvaluate = function() {
+		for(i=0; i<dArray.length; i++) {
+			// Iterate through each element of array, performing function depending on input
+		}
+
+	}
 	
 
 });
